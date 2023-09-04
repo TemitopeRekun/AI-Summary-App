cb15188a93291a4861d4873aba551a426bd12d4b
TemitopeOgunrekun <olalekanogunrekun@gmail.com>
2023-09-04 23:09:48 +0100

    styling the input field

--- src/component/Demo.jsx
+++ src/component/Demo.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+
+import { tick, copy, linkIcon, loader } from "../assets";
 
 const Demo = () => {
-  return (
-    <div>Demo</div>
-  )
-}
+	return (
+		<section className="mt-16 w-full max-w-xl">
+			<div className="flex flex-col w-full gap-2">
+				<form
+					className="relative flex justify-center item-center"
+					onSubmit={() => {}}>
+					<img
+						src={linkIcon}
+						alt="link_icon"
+						className="absolute left-0 my-3 ml-3 w-5"
+					/>
+					<input
+						type="url"
+						placeholder="Enter a URL"
+						value=""
+						onChange={() => {}}
+						required
+						className="url_input peer"
+					/>
 
-export default Demo
+					<button
+						type="submit"
+						className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700">
+						{" "}
+						Enter
+					</button>
+				</form>
+			</div>
+		</section>
+	);
+};
+
+export default Demo;
